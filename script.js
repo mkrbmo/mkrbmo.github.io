f8bc5a5de920a4258c02ceff43214382c45f1495
Miles <mkrbmo@gmail.com>
2022-12-04 13:57:53 -0500

    simplified index and styling; version 1 for deployment

--- script.js
+++ script.js
@@ -12,8 +12,30 @@ document.addEventListener('scroll', function (event) {
     
     moon.style.left = window.innerWidth*ratio-moon.offsetWidth/2+'px'
     moon.style.top = (2*ratio**2 - 2*ratio + 0.5)*100+'%'
-    
-    
 })
-message.innerHTML = Math.max( body.scrollHeight, body.offsetHeight, 
-    html.clientHeight, html.scrollHeight, html.offsetHeight ) - window.innerHeight;
+function incrementSlide(n) {
+    slide = document.querySelector('.active-image').id;
+    slide_number = +slide.slice(-1);
+    
+    if (n == -1 && slide_number > 1) {
+        image = document.getElementById(slide);
+        image.classList.toggle('active-image');
+
+        image = document.getElementById('image'+(slide_number-1).toString());
+        image.classList.toggle('active-image');
+        
+
+    } else if (n == 1 && document.getElementById('image'+(slide_number+1).toString())) {
+        image = document.getElementById(slide);
+        image.classList.toggle('active-image');
+
+        image = document.getElementById('image'+(slide_number+1).toString());
+        image.classList.toggle('active-image');
+    }
+
+    
+
+    
+    
+    
+}
